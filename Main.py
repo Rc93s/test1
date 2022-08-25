1d1aa09c89c32c0a9deb559ede1697a3cb99678e
Rc93s <cohenroy93@gmail.com>
2022-08-25 13:11:13 +0300

    feat: add pint command from roy

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 print("button1")
 print("test pull")
 print("this is also a test")
-
+print ("Mike and Libby") # add from roy
 username = input("enter username: \n")
 print("\n")
 print("hello {}".format(username))
